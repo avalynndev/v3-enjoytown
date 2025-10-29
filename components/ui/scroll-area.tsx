e35a6c915693e618f83c4beb0fe70c9de6f954d5
Avalynn <avalynndev@gmail.com>
2025-10-29 06:36:59 +0530

    fixed scroll

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -38,11 +38,11 @@ function ScrollBar({
       data-slot="scroll-area-scrollbar"
       orientation={orientation}
       className={cn(
-        "flex touch-none p-px transition-colors select-none hidden",
+        "touch-none p-px transition-colors select-none hidden", //flex
         orientation === "vertical" &&
           "h-full w-2.5 border-l border-l-transparent",
         orientation === "horizontal" &&
-          "h-0 hidden flex-col border-t border-t-transparent",
+          "h-2.5 flex-col border-t border-t-transparent",
         className,
       )}
       {...props}
