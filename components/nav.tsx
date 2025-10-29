ad422c3ceaded2fee20903297921c14d0914ea26
Avalynn <avalynndev@gmail.com>
2025-10-29 21:45:32 +0530

    prettier

--- components/nav.tsx
+++ components/nav.tsx
@@ -140,7 +140,7 @@ export default function Navbar() {
                       />
                       {label}
                     </SelectItem>
-                  )
+                  ),
                 )}
               </SelectContent>
             </Select>
